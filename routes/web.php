2c7662570b55812264711dc51e7849a1638d4c8c
sajidahfitriyani <sajidahfitriyani5@gmail.com>
2024-12-12 14:18:34 +0700

    Mengubah PHP dan Routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+
+Route::get('/ResepObat', function () {
+    return view('resepobat');
+});
+
+Route::get('/JadwalDokter', function () {
+    return view('jadwaldokter');
+});
