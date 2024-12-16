68e2711a4e43366c9ccef6fb70b6c2a3ea322910
Tanuki74 <bannyjuanda@gmail.com>
2024-12-16 10:42:50 +0700

    ubah javascript

--- resources/views/RegistrasiPasien/create.blade.php
+++ resources/views/RegistrasiPasien/create.blade.php
@@ -52,4 +52,22 @@
         </div>
     </form>
 </div>
+<script>
+function filterDokter() {
+    const jenisLayanan = document.querySelector('input[name="jenisLayanan"]:checked').value;
+    const dokterOptions = document.querySelectorAll('.dokter-option');
+
+    // Iterasi melalui semua option dokter dan tampilkan hanya yang sesuai
+    dokterOptions.forEach(option => {
+        if (option.getAttribute('data-spesialis') === jenisLayanan) {
+            option.style.display = 'block'; // Tampilkan option
+        } else {
+            option.style.display = 'none'; // Sembunyikan option
+        }
+    });
+
+    // Reset pilihan dokter saat layanan diubah
+    document.getElementById('jadwal_id').selectedIndex = 0;
+}
+</script>
 @endsection
